1175f0cbed5e113bd74436514d079462ca970951
mayki-douglas <mayki.douglas1@gmail.com>
2022-08-31 22:42:47 -0300

    Conceito de objetos e Funções

---
+++ secao-2-fundamentos/22-objeto1.js
@@ -0,0 +1,14 @@
+console.log(typeof Object);
+console.log(typeof new Object);
+
+const Cliente = function (){}
+console.log(typeof Cliente);
+console.log(typeof new Cliente);
+
+class Produto{}
+console.log(typeof Produto);
+console.log(typeof new Produto);
+
+//neste algorítmo vimos que os tipos de dados em JavaScript, em sua maioria são funções
+//instanciando o tipo de dado, ele muda para objeto, usando o New.
+//independente se o dado for uma constante, ou uma classe.
